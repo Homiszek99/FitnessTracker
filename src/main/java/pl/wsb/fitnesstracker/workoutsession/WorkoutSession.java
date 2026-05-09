151674dec7a5d61a1d8f336fee7e5581eccdf384
Homiszek99 <homakarol@wp.pl>
2026-05-09 15:37:46 +0200

    Poprawki w WorkoutSession.java (przechodzi testy!)

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,6 +1,5 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -8,10 +7,10 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import pl.wsb.fitnesstracker.training.api.Training;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "workoutsession")
+@Table(name = "workout_session")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
@@ -19,40 +18,16 @@ public class WorkoutSession {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Nullable
     private Long id;
-    private java.time.LocalDateTime timestamp;
-
 
     @ManyToOne
-    @JoinColumn(name = "training_id", nullable = false)
+    @JoinColumn(name = "training_id")
     private Training training;
 
-    @Column(name = "startLatitude", nullable = false)
-    private Date startLatitude;
-
-    @Column(name = "startLongitude", nullable = false)
-    private Date startLongitude;
-
-    @Column(name = "endLatitudee", nullable = false)
-    private Date endLatitude;
-
-    @Column(name = "endLongitude", nullable = false)
-    private Date endLongitude;
-
-    @Column(name = "altitude", nullable = false)
-    private int altitude;
-
-    public WorkoutSession(
-            final Date startLatitude,
-            final Date startLongtitude,
-            final Date endLatitude,
-            final Date endLongitude,
-            final int altitude) {
-        this.startLatitude = startLatitude;
-        this.startLongitude = startLongtitude;
-        this.endLatitude = endLatitude;
-        this.endLongitude = endLongitude;
-        this.altitude = altitude;
-    }
+    private LocalDateTime timestamp;
+    private double startLatitude;
+    private double startLongitude;
+    private double endLatitude;
+    private double endLongitude;
+    private double altitude;
 }
